767635b208a7d85e9d8fc2595e16dca3a65db272
JasonD21 <jasondavids54@gmail.com>
2026-06-10 18:08:22 +0200

    Chunk 2: data + tenancy foundation (entities, DbContext, migration)

--- src/Api/AiSupportAgent.Api/Program.cs
+++ src/Api/AiSupportAgent.Api/Program.cs
@@ -1,4 +1,25 @@
+using AiSupportAgent.Api.Common;
+using AiSupportAgent.Api.Identity;
+using AiSupportAgent.Api.Persistence;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ITenantContext, TenantContext>();
+
+builder.Services.AddDbContext<AppDbContext>((sp, options) => options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Default"),
+        npgsql => npgsql.UseVector())
+);
+
+builder.Services.AddDataProtection().PersistKeysToDbContext<AppDbContext>();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options =>
+    {
+        options.Password.RequiredLength = 8;
+        options.User.RequireUniqueEmail = true;
+    }).AddEntityFrameworkStores<AppDbContext>();
 
 const string DevCorsPolicy = "DevCors";
 builder.Services.AddCors(options =>
